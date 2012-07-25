efb340f799fe001d5aa2f3751e141a21277bc075
unknown <orihaus@gmail.com>
2012-07-26 02:35:48 +1200

    OZML Export

--- Assets/Scripts/OZMLSave.cs
+++ Assets/Scripts/OZMLSave.cs
@@ -5,15 +5,42 @@ using System.Xml;
 public class OZMLSave : MonoBehaviour 
 {
 	public string LevelName = "dori0";
+	public Material[] SceneMaterials;
 	
 	void Start() 
 	{
 		XmlWriter writer = XmlWriter.Create( LevelName + ".xml" );
 		writer.WriteStartDocument();
+		writer.WriteStartElement("ozml");
+		
+		// Scene
+		writer.WriteStartElement("scene");
+		
+		string background = GetComponentInChildren<Camera>().backgroundColor.ToString();
+		background = background.Substring( 5, background.Length - 13 );
+		writer.WriteAttributeString( "background", background );
+		
+		string fog = RenderSettings.fogColor.ToString();
+		fog = fog.Substring( 5, fog.Length - 6 );
+		writer.WriteAttributeString( "fog", fog );
+			
+		writer.WriteEndElement();
+		writer.Flush();
+		// Materials
+		writer.WriteStartElement("materials");
+		
+		foreach( Material mat in SceneMaterials )
+		{
+			writer.WriteRaw( "." + mat.name + "{texture:http://dev.xxiivv.com/ozml/img/" + mat.name + ".jpg;}");
+		}
+		
+		writer.WriteEndElement();
+		writer.Flush();
+		
+		// Geometry
 		writer.WriteStartElement("geometry");
 		
 		GameObject[] cubes = GameObject.FindGameObjectsWithTag( "Cube" );
-		
 		for( int i = 0; i < cubes.Length; i++ )
 	    {
 			GameObject cube = cubes[i];
@@ -32,6 +59,8 @@ public class OZMLSave : MonoBehaviour
 			
 			writer.Flush();
 	    }
+		
+		writer.WriteEndElement();
 		
 		writer.WriteEndElement();
 		writer.WriteEndDocument();
